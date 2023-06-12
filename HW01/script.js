6767b50d4c7ad2f0674b139d07686c473d3712c6
Tatka751 <135678682+Tatka751@users.noreply.github.com>
2023-06-12 19:15:14 +0300

    Update script.js

--- HW01/script.js
+++ HW01/script.js
@@ -1,48 +1,21 @@
-let priceHeadphone = 15.678;
-let priceTablet = 123.965;
-let priceLaptop = 90.2345;
+const priceHeadphone = 15.678;
+const priceTablet = 123.965;
+const priceLaptop = 90.2345;
+const min = 3;
+const max = 15;
 let maxPrice = Math.max(priceHeadphone, priceTablet,priceLaptop);
 let minPrice = Math.min(priceHeadphone, priceTablet,priceLaptop);
 let sumPrice = Number(priceHeadphone+priceTablet+priceLaptop);
-let truncSumPrice = Math.trunc(sumPrice);
-let ceilSumPrice = Math.ceil(sumPrice/100)*100;
-let fSumPrice = Math.floor(sumPrice/100)*100;
-
-console.log('Ціна навушників: '+priceHeadphone);
-console.log('Ціна планшета: '+ priceTablet);
-console.log('Ціна ноутбука: '+ priceLaptop);
-console.log('Найбільша ціна з цих товарів: '+ maxPrice);
-console.log('Найменша ціна з цих товарів: '+ minPrice);
-console.log('Загальна ціна товарів: '+ sumPrice);
-console.log('Загальна ціна товарів без копійок: '+ truncSumPrice );
-console.log('Сума товарів з округленням до сотень (в більшу сторону): '+ ceilSumPrice);
-console.log('Сума товарів з округленням до сотень (в меншу сторону): '+ fSumPrice);
-
+let truncSumPrice = Math.trunc(priceHeadphone) + Math.trunc(priceTablet) + Math.trunc(priceLaptop);
+let ceilSumPrice = Math.ceil(sumPrice / 100) * 100;
+let fSumPrice = Math.floor(sumPrice / 100) * 100;
 let floorSumPrice = Math.floor(sumPrice)
 let evenNumber ;
-evenNumber = Boolean(floorSumPrice%2);
-console.log('сума всіх товарів,округлена в меншу сторону, є непарним числом? ' +evenNumber);
-
 let restOfMoney = 500 - sumPrice;
-console.log('Решта після оплати купюрою 500 грн становитиме ' +restOfMoney);
-
-let averageSum = Math.ceil((sumPrice/3)*100)/100;
-console.log('Середня ціна становить  ' +averageSum);
-
-/////////**************** */
-const min = 3;
-const max = 15;
-let discount = Math.ceil((max-min)*Math.random()+min);
-console.log('Знижка становить  ' +discount+' %');
-
-let sumToPay = Math.ceil(((sumPrice * ((100-discount)*0.01))*100))/100;
-console.log('Сума до оплати за всі товари з урахуванням знижки  ' +sumToPay);
-
-
-
-let profit = Math.ceil((sumPrice/2-(discount*0.01*sumPrice))*100)/100;
-console.log('Чистий прибуток:   ' +profit); 
-
+let averageSum = Math.ceil((sumPrice / 3) * 100) / 100;
+let discount = Math.ceil((max - min) * Math.random() + min);
+let sumToPay = Math.ceil(((sumPrice * ((100 - discount) * 0.01)) * 100)) / 100;
+let profit = Math.ceil((sumPrice / 2 - (discount * 0.01 * sumPrice)) * 100) / 100;
 var text = `Найбільша ціна з цих товарів: ${maxPrice} <br>
 Найменша ціна з цих товарів: ${minPrice} <br>
 Загальна ціна товарів: ${sumPrice} <br>
@@ -55,6 +28,24 @@ Cума всіх товарів,округлена в меншу сторону,
 Знижка становить ${discount} % <br>
 Сума до оплати за всі товари з урахуванням знижки ${sumToPay} <br> 
 Чистий прибуток: ${profit} <br>  `;
+
+console.log('Ціна навушників: '+priceHeadphone);
+console.log('Ціна планшета: '+ priceTablet);
+console.log('Ціна ноутбука: '+ priceLaptop);
+console.log('Найбільша ціна з цих товарів: '+ maxPrice);
+console.log('Найменша ціна з цих товарів: '+ minPrice);
+console.log('Загальна ціна товарів: '+ sumPrice);
+console.log('Загальна ціна товарів без копійок: '+ truncSumPrice);
+console.log('Сума товарів з округленням до сотень (в більшу сторону): '+ ceilSumPrice);
+console.log('Сума товарів з округленням до сотень (в меншу сторону): '+ fSumPrice);
+
+evenNumber = Boolean(floorSumPrice % 2);
+console.log('сума всіх товарів,округлена в меншу сторону, є непарним числом? ' +evenNumber);
+console.log('Решта після оплати купюрою 500 грн становитиме ' +restOfMoney);
+console.log('Середня ціна становить  ' +averageSum);
+console.log('Знижка становить  ' + discount + ' %');
+console.log('Сума до оплати за всі товари з урахуванням знижки  ' +sumToPay);
+console.log('Чистий прибуток:   ' +profit); 
 
 document.write(text)
 
