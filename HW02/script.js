c2df8a81acfb0027c0f2f8b5112e9876a0545f01
Tatka751 <135678682+Tatka751@users.noreply.github.com>
2023-06-20 06:20:20 +0300

    Update script.js

--- HW02/script.js
+++ HW02/script.js
@@ -1,32 +1,31 @@
 let SUM = 0;
 let N = Number(prompt('Введіть число, від якого будемо складати '));
-if (Number.isInteger(N) && !isNaN(N))
-{
-    let M = Number(prompt('Введіть число, по яке будемо складати якого будемо складати ')); 
-}
-if (Number.isInteger(M) && !isNaN(M))
-{
-  let C = confirm('Чи потрібно пропускати парні числа?');
-    document.writeln(`Число, від якого складали :        ${N} <br>`);
-
-for (N; N <= M; N++){
-       if (C === false){
-            SUM = SUM + N;
-       }
-        else {
-            if (N % 2 > 0){
-                SUM = SUM + N;
+if (Number.isInteger(N) && isNaN(N) == false){
+    const nWrite = N;
+    let M = Number(prompt('Введіть число, по яке будемо складати якого будемо складати '));
+    if (Number.isInteger(M) && isNaN(M) == false && M>N){
+        let C = confirm('Чи потрібно пропускати парні числа?');
+        for (N; N <= M; N++){
+            if (C === false){
+                 SUM = SUM + N;
             }
-            else {
+             else{
+                 if (N % 2 > 0){
+                     SUM = SUM + N;
+                 }
+                 else {
+     
+                 }
+             }
+         }
+         document.writeln(`Число, від якого складали :        ${nWrite} <br>`);
+         document.writeln(`Число, по яке складали :           ${M} <br>`);
+         document.writeln(`Чи були пропущені парні числа :    ${C} <br>`);
+         document.writeln(`Число, отрмиане після складання :  ${SUM} <br>`);
 
-            }
-     }
-    
-    
-    document.writeln(`Число, по яке складали :           ${M} <br>`);
-    document.writeln(`Чи були пропущені парні числа :    ${C} <br>`);
-    document.writeln(`Число, отрмиане після складання :  ${SUM} <br>`);
-} else 
+    }
+} else{
     document.writeln(`Складання не виконано. Перевірте, щоб введені числа були цілими.`);
-
 }
+
+
