1692fb65e5da8413996f4ee4b58fe8a8749a5a21
Tatka751 <135678682+Tatka751@users.noreply.github.com>
2023-06-20 05:47:26 +0300

    Update script.js

--- HW02/script.js
+++ HW02/script.js
@@ -1,24 +1,28 @@
 let SUM = 0;
 let N = Number(prompt('Введіть число, від якого будемо складати '));
-let M = Number(prompt('Введіть число, по яке будемо складати якого будемо складати '));
-let C = confirm('Чи потрібно пропускати парні числа?');
+if (Number.isInteger(N) && !isNaN(N))
+{
+    let M = Number(prompt('Введіть число, по яке будемо складати якого будемо складати ')); 
+}
+if (Number.isInteger(M) && !isNaN(M))
+{
+  let C = confirm('Чи потрібно пропускати парні числа?');
+}
 document.writeln(`Число, від якого складали :        ${N} <br>`);
 
-if (Number.isInteger(N) && Number.isInteger(M) && (M > N) && isNaN(N) == false && isNaN(M) == false)
-{
-     for (N; N <= M; N++){
+for (N; N <= M; N++){
        if (C === false){
             SUM = SUM + N;
        }
-        else{
+        else {
             if (N % 2 > 0){
                 SUM = SUM + N;
             }
             else {
 
             }
-        }
-    }
+     }
+    
     
     document.writeln(`Число, по яке складали :           ${M} <br>`);
     document.writeln(`Чи були пропущені парні числа :    ${C} <br>`);
