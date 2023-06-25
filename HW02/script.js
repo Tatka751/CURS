37789bebb243ea7906aa1b5cd36436c0a27c5f75
tetiana <tetyanaomeliuta@gmail.com>
2023-06-25 11:08:20 +0300

    commit

--- HW02/script.js
+++ HW02/script.js
@@ -1,31 +1,32 @@
 let SUM = 0;
-let N = Number(prompt('Введіть число, від якого будемо складати '));
-if (Number.isInteger(N) && isNaN(N) == false){
-    const nWrite = N;
-    let M = Number(prompt('Введіть число, по яке будемо складати якого будемо складати '));
-    if (Number.isInteger(M) && isNaN(M) == false && M>N){
-        let C = confirm('Чи потрібно пропускати парні числа?');
-        for (N; N <= M; N++){
-            if (C === false){
-                 SUM = SUM + N;
+let N = 0;
+do {
+    N = prompt('Введіть число, від якого будемо складати ');
+    N = Number(N);
+    } while (isNaN(N) || Number.isInteger(N) ===false);
+let nWrite = N;
+let M = 0;
+do {
+    M = prompt('Введіть число, до якого будемо складати ');
+    M = Number(M);
+    } while (isNaN(M) || Number.isInteger(M) === false  || M <= N);
+
+let C = confirm('Чи потрібно пропускати парні числа?');
+    for (N; N <= M; N++){
+        if (C === false){
+            SUM = SUM + N;
             }
              else{
                  if (N % 2 > 0){
                      SUM = SUM + N;
                  }
-                 else {
-     
-                 }
+                
              }
          }
-         document.writeln(`Число, від якого складали :        ${nWrite} <br>`);
-         document.writeln(`Число, по яке складали :           ${M} <br>`);
-         document.writeln(`Чи були пропущені парні числа :    ${C} <br>`);
-         document.writeln(`Число, отрмиане після складання :  ${SUM} <br>`);
+document.writeln(`Число, від якого складали :        ${nWrite} <br>`);
+document.writeln(`Число, по яке складали :           ${M} <br>`);
+document.writeln(`Чи були пропущені парні числа :    ${C} <br>`);
+document.writeln(`Число, отрмиане після складання :  ${SUM} <br>`);
 
-    }
-} else{
-    document.writeln(`Складання не виконано. Перевірте, щоб введені числа були цілими.`);
-}
 
 
